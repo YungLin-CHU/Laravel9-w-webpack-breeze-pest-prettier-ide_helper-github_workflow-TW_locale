4b17ff05cc9b2e151959458dfd9f359b0fe6b484
YungLin Chu <lexlie.yunglinchu@gmail.com>
2022-08-29 01:29:21 +0800

    feat: add prettier formatter and format workflow

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -14,13 +15,14 @@
     <link rel="stylesheet" href="{{ mix('css/app.css') }}">
     <script src="{{ mix('js/app.js') }}" defer></script>
 </head>
+
 <body class="font-sans antialiased">
 <div class="min-h-screen bg-gray-100">
     @include('layouts.navigation')
 
     <!-- Page Heading -->
     <header class="bg-white shadow">
-        <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+        <div class="mx-auto max-w-7xl py-6 px-4 sm:px-6 lg:px-8">
             {{ $header }}
         </div>
     </header>
@@ -31,4 +33,5 @@
     </main>
 </div>
 </body>
+
 </html>
